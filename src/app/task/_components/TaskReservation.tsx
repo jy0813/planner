a130501266f6f70a19ede3eaf2c13ace4cae0869
jy0813 <busbug3@gmail.com>
2024-03-15 16:43:33 +0900

    Feat: 테스크 리사이즈 이벤트 추카

--- src/app/task/_components/TaskReservation.tsx
+++ src/app/task/_components/TaskReservation.tsx
@@ -1,49 +1,104 @@
 "use client";
 
+import { MouseEventHandler, useMemo } from "react";
+
 import styles from "./TaskReservation.module.scss";
+import getHours from "@/utils/getHours";
+
+const MIN_WIDTH = 100;
 
 const TaskReservation = () => {
+  const hours = useMemo(() => {
+    return getHours();
+  }, []);
+
+  const dragHandler: MouseEventHandler<HTMLDivElement> = (e) => {
+    const target = e.currentTarget.parentElement;
+    if (!target) return;
+
+    const initialLeft = target.getBoundingClientRect().left;
+
+    const resize: EventListener = (e) => {
+      const left = (e as MouseEvent).clientX;
+      const width = Math.max(MIN_WIDTH, left - initialLeft);
+      target.style.width = `${width / 10}rem`;
+
+      console.log(initialLeft, left, width);
+    };
+
+    document.addEventListener("mousemove", resize);
+    document.addEventListener(
+      "mouseup",
+      () => document.removeEventListener("mousemove", resize),
+      { once: true }
+    );
+  };
+
+  console.log(hours);
   return (
     <div className={styles.reservationWrap}>
-      <div className={styles.reservationHeader}>
-        <h3>예약</h3>
-      </div>
-      <div className={styles.reservationBody}>
-        <div className={styles.reservationTimeArea}>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
-          <div>7</div>
-          <div>8</div>
-          <div>9</div>
-          <div>10</div>
+      <div className={styles.reservationTaskArea}>
+        <div className={styles.reservationTaskHeaderContent}>
+          <div className={styles.taskHeader}>
+            <span>예약</span>
+            <div
+              onMouseDown={dragHandler}
+              className={styles.resizeDragArea}
+            ></div>
+          </div>
+          <div className={styles.taskHeader}>
+            <span>접수</span>
+            <div className={styles.resizeDragArea}></div>
+          </div>
+          <div className={styles.taskHeader}>
+            <span>검사</span>
+            <div className={styles.resizeDragArea}></div>
+          </div>
+          <div className={styles.taskHeader}>
+            <span>진료</span>
+            <div className={styles.resizeDragArea}></div>
+            <div className={styles.subTaskHeaderArea}>
+              <div className={styles.subTaskHeader}>
+                <span>의사1</span>
+              </div>
+              <div className={styles.subTaskHeader}>
+                <span>의사2</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.taskHeader}>
+            <span>치료</span>
+            <div className={styles.resizeDragArea}></div>
+            <div className={styles.subTaskHeaderArea}>
+              <div className={styles.subTaskHeader}>
+                <span>원적외선</span>
+              </div>
+              <div className={styles.subTaskHeader}>
+                <span>온열</span>
+              </div>
+              <div className={styles.subTaskHeader}>
+                <span>VIP 1</span>
+              </div>
+              <div className={styles.subTaskHeader}>
+                <span>VIP 2</span>
+              </div>
+              <div className={styles.subTaskHeader}>
+                <span>VIP 3</span>
+              </div>
+              <div className={styles.subTaskHeader}>
+                <span>VIP 4</span>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.reservationTimeArea}>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
-          <div>7</div>
-          <div>8</div>
-          <div>9</div>
-          <div>10</div>
-        </div>
-        <div className={styles.reservationTimeArea}>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
-          <div>7</div>
-          <div>8</div>
-          <div>9</div>
-          <div>10</div>
+        <div className={styles.reservationTaskBodyContent}>
+          <div className={styles.reservationTimeArea}>
+            {hours.map((hour, index) => (
+              <div key={index} className={styles.reservationTime}>
+                {hour}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
---
+++ src/utils/getHours.ts
@@ -0,0 +1,20 @@
+const getHours = (start = 0, end = 24, interval = 15) => {
+  const hours = [];
+
+  for (let i = start; i < end; i++) {
+    for (let j = 0; j < 60; j += interval) {
+      const minutes = j.toString().padStart(2, "0");
+      if (i === 0) {
+        hours.push(`오전 12:${minutes}`);
+      } else if (i > 12) {
+        hours.push(`오후 ${i - 12}:${minutes}`);
+      } else {
+        hours.push(`오전 ${i}:${minutes}`);
+      }
+    }
+  }
+
+  return hours;
+};
+
+export default getHours;
