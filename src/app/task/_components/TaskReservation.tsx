38f174b02d1a0bd591d4406e6bdcaf37cd404909
jy0813 <busbug3@gmail.com>
2024-03-20 14:02:33 +0900

    Feat: Task Dnd 구현

--- src/app/task/_components/TaskReservation.tsx
+++ src/app/task/_components/TaskReservation.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-import { MouseEventHandler, useEffect, useMemo, useRef } from "react";
+import {
+  DragEvent,
+  MouseEventHandler,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 
 import styles from "./TaskReservation.module.scss";
 import getHours from "@/utils/getHours";
-import { useQueries, useQuery } from "@tanstack/react-query";
+import { useQueries } from "@tanstack/react-query";
 import { getTaskData } from "@/service/getTaskData";
 import { TaskData } from "@/types/task";
 import { getClinicBusinessTimeDate } from "@/service/getClinicBusinessTimeData";
@@ -12,6 +19,8 @@ import { getClinicBusinessTimeDate } from "@/service/getClinicBusinessTimeData";
 const MIN_WIDTH = 100;
 
 const TaskReservation = () => {
+  const dragItem = useRef<number | null>(null);
+  const dragOverItem = useRef<number | null>(null);
   const parentRef = useRef<HTMLDivElement>(null);
 
   const [{ data: taskData }, { data: businessTimeData }] = useQueries({
@@ -31,6 +40,8 @@ const TaskReservation = () => {
     ],
   });
 
+  const [managedTaskData, setManagedTaskData] = useState(taskData);
+
   const hours = useMemo(() => {
     if (
       businessTimeData?.businessStartTime ||
@@ -45,6 +56,10 @@ const TaskReservation = () => {
   }, [businessTimeData?.businessEndTime, businessTimeData?.businessStartTime]);
 
   useEffect(() => {
+    setManagedTaskData(taskData);
+  }, [taskData]);
+
+  useEffect(() => {
     if (parentRef.current) {
       const children = Array.from(parentRef.current.children) as HTMLElement[];
       for (let i = 0; i < children.length; i++) {
@@ -55,14 +70,21 @@ const TaskReservation = () => {
         }
       }
     }
-  }, [taskData]);
+  }, [managedTaskData]);
 
-  const dragHandler: MouseEventHandler<HTMLDivElement> = (e) => {
+  const dragResizeHandler: MouseEventHandler<HTMLDivElement> = (e) => {
     const target = e.currentTarget.parentElement;
 
     if (!target) return;
 
     const initialLeft = target.getBoundingClientRect().left;
+
+    if (parentRef.current) {
+      const children = Array.from(parentRef?.current.children) as HTMLElement[];
+      children.forEach((child) => {
+        child.style.pointerEvents = "none";
+      });
+    }
 
     const resize: EventListener = (e) => {
       const left = (e as MouseEvent).clientX;
@@ -74,23 +96,60 @@ const TaskReservation = () => {
     };
 
     document.addEventListener("mousemove", resize);
-    document.addEventListener(
-      "mouseup",
-      () => document.removeEventListener("mousemove", resize),
-      { once: true }
-    );
+    document.addEventListener("mouseup", () => {
+      document.removeEventListener("mousemove", resize), { once: true };
+      if (parentRef.current) {
+        const children = Array.from(
+          parentRef?.current.children
+        ) as HTMLElement[];
+        children.forEach((child) => {
+          child.style.pointerEvents = "auto";
+        });
+      }
+    });
   };
 
+  const dragStartHandler = (e: DragEvent<HTMLDivElement>, position: number) => {
+    dragItem.current = position;
+    console.log((e.target as HTMLDivElement).innerHTML);
+  };
+
+  const dragEndHandler = (e: DragEvent<HTMLDivElement>, position: number) => {
+    dragOverItem.current = position;
+    console.log((e.target as HTMLDivElement).innerHTML);
+  };
+
+  const drop = () => {
+    const newTaskData = [...managedTaskData];
+    const dragItemValue =
+      dragItem.current !== null ? newTaskData[dragItem.current] : null;
+    if (dragItem.current !== null) {
+      newTaskData.splice(dragItem.current, 1);
+    }
+    newTaskData.splice(dragOverItem.current ?? 0, 0, dragItemValue);
+    dragItem.current = null;
+    dragOverItem.current = null;
+    console.log(newTaskData);
+    setManagedTaskData(newTaskData);
+  };
   return (
     <div className={styles.reservationWrap}>
       <div className={styles.reservationTaskArea}>
         <div ref={parentRef} className={styles.reservationTaskHeaderContent}>
-          {taskData?.map((task: TaskData) => {
+          {managedTaskData?.map((task: TaskData, index: number) => {
             return (
-              <div key={task.id} className={styles.taskHeader}>
+              <div
+                key={task.id}
+                className={styles.taskHeader}
+                draggable
+                onDragStart={(e) => dragStartHandler(e, index)}
+                onDragEnter={(e) => dragEndHandler(e, index)}
+                onDragEnd={drop}
+                onDragOver={(e) => e.preventDefault()}
+              >
                 <span>{task.name}</span>
                 <div
-                  onMouseDown={dragHandler}
+                  onMouseDown={dragResizeHandler}
                   className={styles.resizeDragArea}
                 ></div>
                 <div className={styles.subTaskHeaderArea}>
