9cd0d5fbf12f20996d533a3e32401d1587ca9e22
jy0813 <busbug3@gmail.com>
2024-03-26 15:42:22 +0900

    Refactor: useDnd 로직 분리 및 배열 순회 로직 수정

--- src/hooks/useDnd.ts
+++ src/hooks/useDnd.ts
@@ -1,6 +1,16 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export const useDnd = <T extends { order: number }>(initialData: T[]) => {
+type DndResult = { order: number; newOrder: number };
+
+export const useDnd = <T extends { order: number }>(
+  initialData: T[]
+): {
+  dragData: T[];
+  onDragStart: (e: React.DragEvent<HTMLDivElement>, order: number) => void;
+  onDragEnter: (e: React.DragEvent<HTMLDivElement>, order: number) => void;
+  onDragOver: (e: React.DragEvent<HTMLDivElement>) => void;
+  onDragEnd: () => DndResult | null;
+} => {
   const [dragData, setDragData] = useState<T[]>(initialData);
   const dragItem = useRef<number | null>(null);
   const dragOverItem = useRef<number | null>(null);
@@ -9,46 +19,45 @@ export const useDnd = <T extends { order: number }>(initialData: T[]) => {
     setDragData(initialData);
   }, [initialData]);
 
-  const onDragStart = useCallback(
-    (e: React.DragEvent<HTMLDivElement>, order: number) => {
-      dragItem.current = order;
-    },
-    []
-  );
+  const onDragStart = (e: React.DragEvent<HTMLDivElement>, order: number) => {
+    dragItem.current = order;
+  };
 
-  const onDragEnter = useCallback(
-    (e: React.DragEvent<HTMLDivElement>, order: number) => {
-      dragOverItem.current = order;
-    },
-    []
-  );
+  const onDragEnter = (e: React.DragEvent<HTMLDivElement>, order: number) => {
+    dragOverItem.current = order;
+  };
 
-  const onDragOver = useCallback((e: React.DragEvent<HTMLDivElement>) => {
+  const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
-  }, []);
+  };
 
-  const onDragEnd = useCallback(() => {
-    if (dragItem.current === null || dragOverItem.current === null) return;
-    const newData = [...dragData];
-    const dragItemValue = newData.find(
+  const swapOrders = (data: T[]): T[] => {
+    const newData = [...data];
+    const dragItemIndex = newData.findIndex(
       (data) => data.order === dragItem.current
     );
-    const dragOverItemValue = newData.find(
+    const dragOverItemIndex = newData.findIndex(
       (data) => data.order === dragOverItem.current
     );
-    if (dragItemValue && dragOverItemValue) {
-      const dragItemIndex = newData.indexOf(dragItemValue);
-      const dragOverItemIndex = newData.indexOf(dragOverItemValue);
 
+    if (dragItemIndex !== -1 && dragOverItemIndex !== -1) {
       newData[dragItemIndex].order = dragOverItem.current ?? 0;
       newData[dragOverItemIndex].order = dragItem.current ?? 0;
-
-      setDragData(newData);
     }
 
-    console.log(dragItem.current, dragOverItem.current);
+    return newData;
+  };
+
+  const onDragEnd = (): DndResult | null => {
+    if (dragItem.current === null || dragOverItem.current === null) {
+      console.error("Drag and drop failed. dragItem or dragOverItem was null.");
+      return null;
+    }
+
+    const newData = swapOrders(dragData);
+    setDragData(newData);
     return { order: dragItem.current, newOrder: dragOverItem.current };
-  }, [dragData]);
+  };
 
   return {
     dragData,
